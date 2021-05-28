8b8db174f4f6f12984924b58ef3b34bbe4654423
ivolz <ivol@criticalblue.com>
2021-05-28 17:31:10 +0100

    Update approov.rb

--- approov.rb
+++ approov.rb
@@ -1,7 +1,7 @@
 class Approov < Formula
   desc "Approov CLI tool"
   homepage "https://github.com/approov/cli"
-  url "https://approov.io"
+  url "https://github.com/approov/homebrew-cli/releases/download/2.7.0/approov-2.7.0.tar.gz"
   sha256 "56d6cf21196cfc38632708143956dcf8d6293c7a3b3dc101836bb758df42d285"
 
   bottle :unneeded
