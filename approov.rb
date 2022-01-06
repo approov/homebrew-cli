531e16621f97f7bb0a995b9c8f104c2c5a280add
jenkins <jenkins@jenkinss-Mac-mini.local>
2022-01-06 14:52:52 +0000

    Update package version to  2.9.0

--- approov.rb
+++ approov.rb
@@ -1,9 +1,9 @@
 class Approov < Formula
   desc "Approov CLI tool"
-  tag  = ""
+  tag  = "2.9.0"
   homepage "https://github.com/approov/cli"
   url "https://github.com/approov/homebrew-cli/releases/download/" + tag + "/approov-" + tag + ".tar.gz"
-  sha256  "730514b07c58065a975431d6b7f8ee36e2e2466246ae0beb87794f2267e1be5a"
+  sha256  "b49659415b0af787eda850049be72634b98997e80c4a81f56067ded3eeb0c067"
 
   bottle :unneeded
 
