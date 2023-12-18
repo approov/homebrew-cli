d26347a8481ae3666e05915bbcd012e38320bcaf
ivo liondov <ivol@ivos-Mac-mini.local>
2023-12-18 16:06:02 +0000

    Add 3.2.0 release files

--- approov.rb
+++ approov.rb
@@ -3,7 +3,7 @@ class Approov < Formula
   tag  = "3.2.0"
   homepage "https://github.com/approov/cli"
   url "https://github.com/approov/homebrew-cli/releases/download/" + tag + "/approov-" + tag + ".tar.gz"
-  sha256  "0205537cd35775abe75c76faa34ab2d1ee9e6f93e12c89d91ba8867ae19bab9e"
+  sha256  "88cd6f95b19e577f8f0badc79effc1300865838d7f79aa638c97128f77d2f974"
 
   #bottle :unneeded
   def install
