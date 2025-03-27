9511e575845553b415d8247f1a66ce21455d93c4
ivo.liondov <ivol@Sanctuary.local>
2025-03-27 09:35:19 +0000

    Update link to homepage

--- approov.rb
+++ approov.rb
@@ -1,7 +1,7 @@
 class Approov < Formula
   desc "Approov CLI tool"
   tag  = "3.4.0"
-  homepage "https://github.com/approov/cli"
+  homepage "https://github.com/approov/homebrew-cli"
   url "https://github.com/approov/homebrew-cli/releases/download/" + tag + "/approov-" + tag + ".tar.gz"
   sha256  "a2c0c079053fbacaff75902d193ebbd91ba123ac8028e1aedb38688b43c992d6"
 
